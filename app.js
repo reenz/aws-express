2673c7b892b4a64a937eba23e40c228907f33a01
Reena <reena.sanjay.sharma@gmail.com>
2018-04-14 19:24:54 +0100

    fixed 502 bad gateway error

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var hello = require('./routes/hello')
+var hello = require('./routes/hello');
 
 var app = express();
-
-app.get('/hello', 'hello.index')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +22,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', hello);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/hello.js
+++ routes/hello.js
@@ -1,4 +1,10 @@
-exports.index = function(req, res) {
-  res.render('hello', {title: 'Hello World'});
- };
+
+ var express = require('express');
+ var router = express.Router();
+ 
+ router.get('/', function(req, res) {
+   res.render('hello', { title: 'Hello World' });
+ });
+ 
+ module.exports = router;
  
